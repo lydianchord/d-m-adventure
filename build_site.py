7546bb63da031c921fed52d0c0a1996e08283861
ahuanguchi <ahuanguchi@users.noreply.github.com>
2015-03-20 10:53:17 -0500

    Moved id to templates

--- build_site.py
+++ build_site.py
@@ -3,17 +3,9 @@ from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader(searchpath='templates'))
 
-game_pages = set((
-    '4internet.html', 'canvasgame.html', 'danmaku.html', 'rps.html'
-))
-
 def render_template(template_name):
     template = env.get_template(template_name)
-    if template_name in game_pages:
-        game_body_id = ' id="gamebody"'
-    else:
-        game_body_id = ''
-    return template_name, template.render(game_body_id=game_body_id)
+    return template_name, template.render()
 
 def to_files(rendered_list):
     for name, text in rendered_list:
