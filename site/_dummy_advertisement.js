a2a5414b496db6fe3922a0a9893caa0361becc97
ahuanguchi <ahuanguchi@users.noreply.github.com>
2015-04-01 18:48:05 -0500

    Updated game over page

--- site/_dummy_advertisement.js
+++ site/_dummy_advertisement.js
@@ -1,1 +1,1 @@
-document.getElementById("dummy").innerHTML = '<div id="feedback">No adblock detected. Don\'t worry, there aren\'t any ads on this site.</div>'
+document.getElementById("dummy").innerHTML = '<div id="feedback">No adblock detected. Don\'t worry, there aren\'t any ads on this site.</div>';
