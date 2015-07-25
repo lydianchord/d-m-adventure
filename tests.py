31f1842e9148f566e93d67b6f1f4b4214111713b
ahuanguchi <ahuanguchi@users.noreply.github.com>
2015-07-25 08:59:21 -0500

    Modified tests

--- tests.py
+++ tests.py
@@ -9,29 +9,42 @@ class SiteTestCase(unittest.TestCase):
     def setUp(self):
         local_app.app.config['TESTING'] = True
         self.app = local_app.app.test_client()
-        self.site_files = [x for x in os.listdir(os.path.join(os.path.dirname(__file__), 'site'))
-                           if not x.startswith('google') and not x.startswith('_')]
+        root = local_app.app.root_path
+        self.site_html = [x for x in os.listdir(os.path.join(root, 'templates'))
+                          if not x.startswith('_')]
+        self.site_static = [x for x in os.listdir(os.path.join(root, 'site'))
+                            if not x.endswith('.html')]
     
     def tearDown(self):
         pass
     
-    def test_get_all_resources(self):
-        assert len(self.site_files)
-        for f in self.site_files:
-            with self.app.get(f, follow_redirects=True) as resp:
-                assert resp.status_code == 200, '%s %s' % (f, resp.status_code)
+    def test_get_index(self):
+        with self.app.get('/') as resp:
+            self.assertEqual(resp.status_code, 200)
+    
+    def test_get_html(self):
+        self.assertGreater(len(self.site_html), 0)
+        for f in self.site_html:
+            with self.app.get('/' + f, follow_redirects=True) as resp:
+                self.assertEqual(resp.status_code, 200, f)
                 if f.endswith('.html'):
                     data = str(resp.get_data())
-                    assert '<!DOCTYPE html>' in data, '%s\n%s' % (f, data)
-                    assert 'href="about.html"' in data, '%s\n%s' % (f, data)
+                    self.assertIn('<!DOCTYPE html>', data, f)
+                    self.assertIn('href="about.html"', data, f)
+    
+    def test_get_static(self):
+        self.assertGreater(len(self.site_html), 0)
+        for f in self.site_static:
+            with self.app.get('/' + f) as resp:
+                self.assertEqual(resp.status_code, 200, f)
     
     def test_not_filename(self):
-        with self.app.get('index') as resp:
-            assert resp.status_code == 404, resp.status_code
+        with self.app.get('/index') as resp:
+            self.assertEqual(resp.status_code, 404)
     
     def test_nonexistent_path(self):
-        with self.app.get('site/index.html') as resp:
-            assert resp.status_code == 404, resp.status_code
+        with self.app.get('/site/index.html') as resp:
+            self.assertEqual(resp.status_code, 404)
 
 
 if __name__ == '__main__':
