c509a4b2528ca50cc06ca44f09a30478ddead7c2
ahuanguchi <ahuanguchi@users.noreply.github.com>
2015-07-25 22:31:44 -0500

    Changed wording, set cache timeout for local app

--- local_app.py
+++ local_app.py
@@ -8,7 +8,8 @@ site_dir = os.path.join(app.root_path, 'site')
 @app.route('/', defaults={'resource': 'index.html'})
 @app.route('/<path:resource>')
 def get_resource(resource):
-    return send_from_directory(site_dir, resource)
+    timeout = 0 if resource.endswith('.html') else None
+    return send_from_directory(site_dir, resource, cache_timeout=timeout)
 
 
 if __name__ == '__main__':
